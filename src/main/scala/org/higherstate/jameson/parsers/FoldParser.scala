9795476dc610c29d099f9a3eee465949a7c08c3d
Jamie Pullar <jpullar@gmail.com>
2013-06-16 16:20:17 +0100

    folding

---
+++ src/main/scala/org/higherstate/jameson/parsers/FoldParser.scala
@@ -0,0 +1,12 @@
+package org.higherstate.jameson.parsers
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Gelfling
+ * Date: 15/06/13
+ * Time: 00:13
+ * To change this template use File | Settings | File Templates.
+ */
+class FoldParser {
+
+}
---
+++ src/test/scala/org/higherstate/jameson/DslStreamParserSpec.scala
@@ -0,0 +1,12 @@
+package org.higherstate.jameson
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Gelfling
+ * Date: 14/06/13
+ * Time: 23:54
+ * To change this template use File | Settings | File Templates.
+ */
+class DslStreamParserSpec {
+
+}
