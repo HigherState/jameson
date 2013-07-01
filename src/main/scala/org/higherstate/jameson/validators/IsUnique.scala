ae5b38c9bbe0f3ef25c05fbb75e73b60c45a541a
Jamie Pullar <jpullar@gmail.com>
2013-07-01 18:50:19 +0100

    started implementing json-schema

---
+++ src/main/scala/org/higherstate/jameson/validators/IsUnique.scala
@@ -0,0 +1,12 @@
+package org.higherstate.jameson.validators
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jamie.pullar
+ * Date: 01/07/13
+ * Time: 16:43
+ * To change this template use File | Settings | File Templates.
+ */
+object IsUnique {
+
+}
